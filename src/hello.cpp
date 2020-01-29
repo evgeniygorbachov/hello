de311c0111e6374ef4404f869f295b08d6a7cea3
Evgeniy_Gorbachov <Evgeniy_Gorbachov@epam.com>
2020-01-29 17:11:53 +0300

    Added conan support and tests

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include "hello/hello.h"
 
-int main()
-{
-    std::cout << "Hello, world!\n";
+namespace hello {
+
+std::ostream& greetings(std::ostream& stream) {
+    return stream << "Hello, world!\n";
 }
+
+} // namespace hello
